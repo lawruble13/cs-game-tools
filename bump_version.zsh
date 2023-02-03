ec7a6eb686fccd3c71b015608f3b74bfaf60cf78
Lauren Wrubleski <lauren@wrubleski.net>
2023-02-03 12:42:56 -0700

    Update bump_version.zsh to reduce version bumps

--- bump_version.zsh
+++ bump_version.zsh
@@ -1,7 +1,14 @@
 #!/usr/bin/env zsh
 
-CURRENT_VERSION=$(grep manifest.json -e '"version"' | sed -E 's/^.*"([0-9]+(\.[0-9]+)*)".*$/\1/')
+gsb_first_line=$(git status -sb | head -n1 | awk '{print $2}')
+upstream_branch=${gsb_first_line#*...}
+if ! git diff ${upstream_branch} manifest.json | grep '"version"' &> /dev/null; then
 
-NEXT_VERSION="${CURRENT_VERSION%.*}.$(( ${CURRENT_VERSION##*.} + 1 ))"
+    CURRENT_VERSION=$(grep manifest.json -e '"version"' | sed -E 's/^.*"([0-9]+(\.[0-9]+)*)".*$/\1/')
 
-sed -i -E "s/(\"version\": )\"${CURRENT_VERSION}\"/\\1\"${NEXT_VERSION}\"/" manifest.json
+    NEXT_VERSION="${CURRENT_VERSION%.*}.$(( ${CURRENT_VERSION##*.} + 1 ))"
+
+    sed -i -E "s/(\"version\": )\"${CURRENT_VERSION}\"/\\1\"${NEXT_VERSION}\"/" manifest.json
+else
+    touch manifest.json
+fi
